2abb366e11abef38af8ce40bd0dc380c32dce826
William So <polyipseity@gmail.com>
2025-08-02 16:02:42 +0800

    Make `mine.py` split inputs for graph miner

--- benchmarks/mine.py
+++ benchmarks/mine.py
@@ -1,10 +1,13 @@
+from dataclasses import dataclass
 from glob import iglob
+from itertools import zip_longest
 from json import dump, load
 from os import name
-from pathlib import Path
+from pathlib import Path, PurePath
 from shutil import move
 from subprocess import check_call
 from sys import stderr, stdout
+from tempfile import TemporaryDirectory
 from time import monotonic_ns
 from types import MappingProxyType
 
@@ -27,9 +30,17 @@ STREAM_MINER = (
 )  # FGC_Stream_CFDMiner [csv_file_path] [minsupp] [window_size] [exit_at]
 CFD_MINER_GRAPH = (
     EXECUTABLE_FOLDER_PATH / f"CFDMiner_Graph{'.exe' if name == 'nt' else ''}"
-)  # CFDMiner_Graph [csv_files_folder] [minsupp] [maxsize]
+)  # CFDMiner_Graph [minsupp] [maxsize] [csv_files_folder...]
+MAX_ITEM_SET_SIZE = 255
 
 INPUT_PATH_PLACEHOLDER = object()
+
+
+@dataclass(frozen=True, kw_only=True, slots=True)
+class PartitionedInputPathsPlaceholder:
+    window: int
+
+
 BENCHMARKS = MappingProxyType(
     {
         **{
@@ -37,7 +48,7 @@ BENCHMARKS = MappingProxyType(
                 CFD_MINER,
                 INPUT_PATH_PLACEHOLDER,
                 str(sup),
-                str(255),
+                str(MAX_ITEM_SET_SIZE),
             )
             for sup in (0.1, 0.05, 0.01, 0.005)
         },
@@ -52,13 +63,14 @@ BENCHMARKS = MappingProxyType(
             for win in (1000, 2000, 5000, 10000)
         },
         **{
-            f"graph, support={sup}": (
+            f"graph, support={sup}, window={win}": (
                 CFD_MINER_GRAPH,
-                INPUT_PATH_PLACEHOLDER,
                 str(sup),
-                str(255),
+                str(MAX_ITEM_SET_SIZE),
+                PartitionedInputPathsPlaceholder(window=win),
             )
             for sup in (0.1, 0.05, 0.01, 0.005)
+            for win in (1000, 2000, 5000, 10000)
         },
     }
 )
@@ -75,23 +87,49 @@ def main() -> None:
                 continue
             result_folder_path.mkdir(exist_ok=True)
 
-            def args0(args=benchmark):
-                for arg in args:
-                    if arg is INPUT_PATH_PLACEHOLDER:
-                        yield data_csv_filepath
-                        continue
-                    if isinstance(arg, (Path, str)):
-                        yield arg
-                        continue
-                    raise ValueError(arg)
+            with TemporaryDirectory() as temp_dir_path:
+                temp_dir_path = Path(temp_dir_path)
 
-            args = tuple(args0())
-            start_time_ns = monotonic_ns()
-            print(f"start: {args}")
-            check_call(args, cwd=cwd, stdin=None, stdout=stdout, stderr=stderr)
-            end_time_ns = monotonic_ns()
-            elapsed_ns = end_time_ns - start_time_ns
-            print(f"end: used {elapsed_ns} ns")
+                def args0(
+                    args=benchmark,
+                    data_csv_filepath=data_csv_filepath,
+                    temp_dir_path=temp_dir_path,
+                ):
+                    temp_file_idx = 0
+                    for arg in args:
+                        if arg is INPUT_PATH_PLACEHOLDER:
+                            yield data_csv_filepath
+                            continue
+                        if isinstance(arg, PartitionedInputPathsPlaceholder):
+                            with data_csv_filepath.open(
+                                "rt", encoding="UTF-8"
+                            ) as data_csv_file:
+                                for lines in zip_longest(
+                                    *((data_csv_file,) * arg.window)
+                                ):
+                                    content = "\n".join(
+                                        line for line in lines if line is not None
+                                    )
+                                    if content:
+                                        temp_filepath = temp_dir_path / str(
+                                            temp_file_idx
+                                        )
+                                        temp_filepath.write_text(content)
+                                        temp_file_idx += 1
+                                        yield temp_filepath
+                            continue
+                        if isinstance(arg, (PurePath, str)):
+                            yield arg
+                            continue
+                        raise ValueError(arg)
+
+                args = tuple(args0())
+                start_time_ns = monotonic_ns()
+                print(f"start: {args}")
+                check_call(args, cwd=cwd, stdin=None, stdout=stdout, stderr=stderr)
+                end_time_ns = monotonic_ns()
+                elapsed_ns = end_time_ns - start_time_ns
+                print(f"end: used {elapsed_ns} ns")
 
             for result_filename in iglob("*.txt", root_dir=cwd):
                 if result_filename == "converted_dataset.txt":
@@ -104,6 +142,10 @@ def main() -> None:
             with performance_filepath.open("r+t", encoding="UTF-8") as performance_file:
                 performance = load(performance_file)
 
+                try:
+                    performance["elapsed"]
+                except KeyError:
+                    performance["elapsed"] = []
                 performance["elapsed"].append(elapsed_ns)
                 performance_file.seek(0)
 
